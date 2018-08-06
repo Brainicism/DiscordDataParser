5a125a576ecbcf851d6fa7634717ad43a4096322
Brian Le <brainicism@gmail.com>
2018-08-05 22:48:27 -0700

    Rename main file

--- discord_data_parser.rb
+++ discord_data_parser.rb
@@ -3,14 +3,14 @@ require 'csv'
 require './analyzer'
 require 'time'
 @analyzer = Analyzer.new
-@@time_zone = Time.now.zone.freeze
+@time_zone = Time.now.zone.freeze
 MESSAGES_PATH = './messages'.freeze
 def parse_csv(file_path, thread_name)
     lines = CSV.read(file_path)
     lines.shift
     lines = lines.map do |line|
         {
-            date_time: Time.parse(line[1]) + Time.zone_offset(time_zone),
+            date_time: Time.parse(line[1]) + Time.zone_offset(@time_zone),
             message: line[2],
             attachments: line[3]
         }
@@ -24,7 +24,6 @@ def write_output(type)
             csv << [key, value]
         end
     end
-
 end
 
 message_index = JSON.parse(File.read("#{MESSAGES_PATH}/index.json"))
@@ -33,6 +32,7 @@ message_index.each do |thread_id, thread_name|
     lines = parse_csv("#{MESSAGES_PATH}/#{thread_id}/messages.csv", thread_name.nil? ? 'unknown_user' : thread_name )
     total_messages = total_messages + lines.length
 end
+
 write_output(:by_date)
 write_output(:by_time_of_day)
 write_output(:by_day_of_week)
